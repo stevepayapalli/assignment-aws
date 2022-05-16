ec87bdcd0c780e9727f83b26ce1ce686edff9912
steve wilson <stevewilson2678@gmail.com>
2022-05-16 23:25:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 
@@ -20,6 +20,5 @@ mongoose.connect("mongodb+srv://Aditya1998:aadi1998@cluster0.zl7lv.mongodb.net/O
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+app.listen(3000)
+    console.log('Express app running on port ' + (3000))
